6bb305b019ea64bd57b9f52df8df452ce663eb84
Josefina Correa Menéndez <jcorream@mit.edu>
2019-03-22 00:51:38 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -5,3 +5,11 @@ def weighed_euclidean_distance(G, beta):
         score = beta * distance.euclidean(neighbor,target)
         return score
     return h
+
+def weighted_degree_centrality(G, alpha, r):
+    def h(neighbor, target):
+        neighbor_graph = nx.ego_graph(G, neighbor, r)
+        degree_centrality = sum(nx.closeness_centrality(neighbor_graph).values())
+        score = alpha * degree_centrality 
+        return score
+    return h
