0748ca752a07917b574d8780d10f0251d98d3bed
Josefina Correa Menéndez <jcorream@mit.edu>
2019-03-22 00:42:15 -0400

    Update test_algorithms.py

--- test_algorithms.py
+++ test_algorithms.py
@@ -15,6 +15,12 @@ beta = 1.0
 source = 0
 target = 100
 
+#test algorith on a simple graph
+path, path_length = astar_path_length(test_graph_nx, 0, 3, alpha, weighed_euclidean_distance(test_graph,beta))
+x=plot_path(test_graph,test_graph_bidirectional, path, path_length, test_graph_node_positions)
+x.show()
+
+#test algorithm on the data
 path, path_length = astar_path_length(data_graph_nx, source, target, alpha = alpha, heuristic = weighed_euclidean_distance(data_graph_nx, beta))
 
 print 'path links: ', len(path)-1
