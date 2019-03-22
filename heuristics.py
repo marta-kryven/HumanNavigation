b67d850899e65cebda24a084f0ea035f73adcd29
Josefina Correa Menéndez <jcorream@mit.edu>
2019-03-22 01:12:10 -0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -1,15 +1,23 @@
 from scipy.spatial import distance
+import networkx as nx
 
-def weighed_euclidean_distance(G, beta):
+def weighted_centrality(G, beta):
     def h(neighbor, target):
-        score = beta * distance.euclidean(neighbor,target)
+        centrality     = sum(list(G.neighbors(neighbor)))
+        score          = (alpha * centrality) 
         return score
     return h
 
-def weighted_degree_centrality(G, alpha, r):
+def weighted_clustering_coefficient(G, alpha, r):
     def h(neighbor, target):
         neighbor_graph = nx.ego_graph(G, neighbor, r)
-        degree_centrality = sum(nx.closeness_centrality(neighbor_graph).values())
-        score = alpha * degree_centrality 
+        clustering_coefficient = sum(nx.clustering(neighbor_graph).values())
+        score = alpha * clustering_coefficient 
+        return score
+    return h
+
+def weighted_euclidean_distance(G, beta):
+    def h(neighbor, target):
+        score = beta * distance.euclidean(neighbor,target)
         return score
     return h
